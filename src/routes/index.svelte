7e30b8e22826c257ea8a9ea752c03ad32211ef05
aiibe <souksavanh.sy@gmail.com>
2021-04-12 11:09:27 +0200

    brand

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,23 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import { title, projects } from '../../raw/site.json';
+	import Logo from '$lib/Logo.svelte'
 </script>
 
 <svelte:head>
-	<title>Hello world!</title>
+	<title>{title}</title>
 </svelte:head>
 
-<main>
-	<h1>Hello world!</h1>
+<nav>
+	<div>
+		<Logo />
+	</div>
+</nav>
 
-	<Counter />
-
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
-</style>
+<div>
+	<h2>Projects</h2>
+	<ul>
+		{#each projects as { name }}
+			<li>{name}</li>
+		{/each}
+	</ul>
+</div>
