92b30f8fbcd07b08978abd0eea94097e043fd712
aiibe <souksavanh.sy@gmail.com>
2021-04-12 12:49:51 +0200

    add

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,9 +1,18 @@
+const sveltePreprocess = require("svelte-preprocess");
 // const node = require('@sveltejs/adapter-node');
 const vercel = require('@sveltejs/adapter-vercel');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	preprocess: [
+		sveltePreprocess({
+			defaults: {
+				style: "postcss",
+			},
+			postcss: true
+		}),
+	],
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
